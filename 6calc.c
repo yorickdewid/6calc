edc684fe9c7838988bd8b8b01568cf92d82decc9
Yorick de Wid <yorick17@outlook.com>
2014-09-16 00:12:38 +0200

    Basic program skeleton

--- 6calc.c
+++ 6calc.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>
 
-int main(int argc, char *agrv[]){
+#define VERSION "v1.0"
+
+/*
+ * Print the usage / help text describing options
+ */
+void usage(char *argv[]){
+    printf(
+        "IPv6 calculator %s\n"
+        "\n"
+        "usage: %s [\033[3mprefix] [subnet]\033[0m\n",
+    VERSION, argv[0]);
+}
+
+int main(int argc, char *argv[]){
+    if(argc < 3)
+        usage(argv);
+
 	return 0;
 }
